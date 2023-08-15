ae622b40f3c78a2a7e38caf8044d6bf3574f21eb
Emmanuel <emmanuel.2001alonso.ec@gmail.com>
2023-08-15 10:26:53 -0500

    making twitter. somehow

---
+++ components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+      <div>Logo</div>
+      <nav>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </nav>
+      <div>User</div>
+    </div>
+  )
+}
+
+export default Sidebar
