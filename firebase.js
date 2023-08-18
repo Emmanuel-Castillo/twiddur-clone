72c8323698238d2c656a6174a14434f32fbb59ab
Emmanuel <emmanuel.2001alonso.ec@gmail.com>
2023-08-18 11:45:48 -0500

    secure api keys

--- firebase.js
+++ firebase.js
@@ -11,12 +11,12 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCdIzBmrdgORlAVkSUu1OEUj82qnV2OSEE",
-  authDomain: "twiddur-c4e97.firebaseapp.com",
-  projectId: "twiddur-c4e97",
-  storageBucket: "twiddur-c4e97.appspot.com",
-  messagingSenderId: "1018301474648",
-  appId: "1:1018301474648:web:3e728c126079430d1da72d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
